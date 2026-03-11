46f1c85782a8d6ba8de348175737a9b9e4c83050
AJT <alex@alexanderjtaylor.com>
2026-03-11 18:44:56 -0400

    granny bazooka

--- src/config.ts
+++ src/config.ts
@@ -24,7 +24,7 @@ export const config: SiteConfig = {
     name: "Alexander J. Taylor",
     bio: "Creative. Poutine Enthusiast.",
 
-    avatar: "/Users/alexanderj.taylor/DAHLDAHL/tokenramem/src/content/blog/pics/peanuthead.jpeg"
+    avatar: "/Users/alexanderj.taylor/DAHLDAHL/tokenramem/public/PFP-website.jpg"
   },
   social: {
     instagram: "https://instagram.com/alexanderjordantaylor",
